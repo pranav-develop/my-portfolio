bf95b4bcc6df1c70b53fb789457553844e1568ec
pranav-develop <pranavschiller@gmail.com>
2021-09-09 14:15:35 +0530

    added navbar and logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,28 @@
 import React from "react";
+import logo from "./../assets/img/pranav.png";
 
 function Navbar() {
     return (
-        <div className={"navbar position-fixed"}>
+        <div className={"nav-bar position-fixed py-1 w-100"}>
             <div className={"container"}>
-                <div>
-
+                <div className={"d-flex w-100 py-1 justify-content-center align-items-center"}>
+                    <div className={"navbar-brand mx-2"}>
+                        <img src={logo} height={"60px"} alt="logo"/>
+                    </div>
+                    <div className={"nav-links d-flex justify-content-end ms-auto w-75 px-5"}>
+                        <a className={"text-decoration-none mx-4 px-2"} href="google.com">
+                            <span className={"text-decoration-none text-uppercase fs-6 ls-2 color-black"}>Home</span>
+                        </a>
+                        <a className={"text-decoration-none mx-4 px-2"} href="google.com">
+                            <span className={"text-decoration-none text-uppercase fs-6 ls-2 color-black"}>About</span>
+                        </a>
+                        <a className={"text-decoration-none mx-4 px-2"} href="google.com">
+                            <span className={"text-decoration-none text-uppercase fs-6 ls-2 color-black"}>Projects</span>
+                        </a>
+                        <a className={"text-decoration-none mx-4 px-2"} href="google.com">
+                            <span className={"text-decoration-none text-uppercase fs-6 ls-2 color-black"}>Contact</span>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,12 @@ import About from "../components/About";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
 import Main from "../components/Main";
+import Navbar from "../components/Navbar";
 
 function Home() {
     return (
         <div className={"home"}>
+            <Navbar />
             <Main />
             <About />
             <Projects />
